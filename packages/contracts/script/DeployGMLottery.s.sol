8836116105d730de15e005950960045e003f4997
Adam Cain <03adamcain@gmail.com>
2025-04-10 13:04:09 +0100

    refactor: update deployment script to use new lottery contracts and enhance environment variable validation

--- packages/contracts/script/DeployGMLottery.s.sol
+++ packages/contracts/script/DeployGMLottery.s.sol
@@ -2,36 +2,49 @@
 pragma solidity ^0.8.28;
 
 import "forge-std/Script.sol";
-import "../src/GMLotteryInterval.sol";
-import "../src/GMLottoNFT.sol";
+import "../src/GMLotteryManager.sol";
+import "../src/GMLotteryToken.sol";
 
 contract DeployGMLottery is Script {
     function run() external {
-        // Get the private key from the environment variable
+        // Get the private key and deployer address from environment variables
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         address deployer = vm.envAddress("DEPLOYER_ADDRESS");
+        address feeRecipient = vm.envAddress("FEE_RECIPIENT");
+        
+        // Validate environment variables
+        require(deployerPrivateKey != 0, "PRIVATE_KEY not set");
+        require(deployer != address(0), "DEPLOYER_ADDRESS not set");
+        require(feeRecipient != address(0), "FEE_RECIPIENT not set");
         
         // Start broadcasting transactions
         vm.startBroadcast(deployerPrivateKey);
 
         // Deploy the NFT contract first
-        GMLottoNFT nft = new GMLottoNFT();
+        GMLotteryToken nft = new GMLotteryToken();
+        console2.log("GMLotteryToken deployed to:", address(nft));
 
-        // Deploy the GMLotto contract with the NFT contract address
-        GMLotto lottery = new GMLotto(deployer, address(nft));
+        // Deploy the lottery manager contract with the NFT contract address
+        GMLotteryManager lottery = new GMLotteryManager(feeRecipient, address(nft));
+        console2.log("GMLotteryManager deployed to:", address(lottery));
 
         // Transfer ownership of the NFT contract to the lottery contract
         nft.transferOwnership(address(lottery));
+        console2.log("NFT ownership transferred to lottery manager");
 
         // Stop broadcasting transactions
         vm.stopBroadcast();
 
-        // Log the deployment details
-        console2.log("GMLottoNFT deployed to:", address(nft));
-        console2.log("GMLotto deployed to:", address(lottery));
-        console2.log("Verify NFT contract with:");
-        console2.log("forge verify-contract", address(nft), "GMLottoNFT --chain optimism-sepolia");
-        console2.log("Verify lottery contract with:");
-        console2.log("forge verify-contract", address(lottery), "GMLotto --chain optimism-sepolia");
+        // Log verification commands
+        console2.log("\nVerification commands:");
+        console2.log("forge verify-contract", address(nft), "GMLotteryToken --chain optimism-sepolia");
+        console2.log("forge verify-contract", address(lottery), "GMLotteryManager --chain optimism-sepolia");
+        
+        // Log important addresses
+        console2.log("\nDeployment Summary:");
+        console2.log("NFT Contract:", address(nft));
+        console2.log("Lottery Manager:", address(lottery));
+        console2.log("Fee Recipient:", feeRecipient);
+        console2.log("Deployer:", deployer);
     }
 } 
