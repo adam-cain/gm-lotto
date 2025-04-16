0b39fcba823f79ac0d32c0bc79a8c9c6ac7e8664
Adam Cain <03adamcain@gmail.com>
2025-04-16 10:16:44 +0100

    style: responsive hero underline thickness

--- packages/frontend/src/app/page.tsx
+++ packages/frontend/src/app/page.tsx
@@ -21,10 +21,10 @@ export default function Home() {
             <h1 className="text-2xl sm:text-5xl md:text-3xl lg:text-5xl xl:text-7xl 2xl:text-8xl font-bold text-white mb-2 sm:mb-3 md:mb-4 2xl:mb-5 text-balance leading-tight">
               <span className="relative inline-block">
                 <span className="relative z-10">Say GM</span>
-                <span className="absolute bottom-1 left-[-4px] right-[-4px] h-2 bg-red-600 z-0"></span>
+                <span className="absolute bottom-1 left-[-4px] right-[-4px] h-2 md:h-3 lg:h-3 xl:h-4 2xl:h-5 bg-red-600 z-0"></span>
               </span> to the Superchain. <span className="relative inline-block">
                 <span className="relative z-10">Win Big</span>
-                <span className="absolute bottom-1 left-[-4px] right-[-4px] h-2 bg-red-600 z-0"></span>
+                <span className="absolute bottom-1 left-[-4px] right-[-4px] h-2 md:h-3 lg:h-3 xl:h-4 2xl:h-5 bg-red-600 z-0"></span>
               </span> Just for Waking Up.
             </h1>
             <p className="text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl text-gray-200">
